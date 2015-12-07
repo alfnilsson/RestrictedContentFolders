f587635b8b0228b8f3ff6741f721efb286bd5bc2
alfnilsson <mail@alfnilsson.se>
2015-12-07 22:46:35 +0100

    Create ContentFolders.cs

---
+++ ContentFolders.cs
@@ -0,0 +1,21 @@
+using System;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Forms.Models.Blocks;
+using EPiServer.Forms.Models.Media;
+
+namespace EPiServer.Forms.Models.Folders
+{
+    [ContentType(GUID = "964063f0-613b-4ce5-85a0-2d11fee0b905")]
+    [AvailableContentTypes(Availability.Specific, Include = new[] { typeof(TeaserBlock) })]
+    public class TeaserContentFolder : ContentFolder
+    {
+    }
+
+    [ContentType(GUID = "e730c883-f9b7-4d5e-b309-a00070159771")]
+    [AvailableContentTypes(Availability.Specific, Include = new[] { typeof(ImageFile) })]
+    public class ImageContentFolder : ContentFolder
+    {
+    }
+}
